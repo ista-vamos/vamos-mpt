57580297be5c66d1121469276cdc9cca6cc1e96c
Ouldouz-Neysari <125090068+Ouldouz-Neysari@users.noreply.github.com>
2023-08-23 15:51:12 +0330

    Add files via upload

---
+++ config.py
@@ -0,0 +1,35 @@
+
+source_dir = "/home/ouldouz/ISTA/TASK 0/vamos/vamos-mpt"
+
+vamos_buffers_DIR         = "/home/ouldouz/ISTA/TASK 0/vamos/vamos-buffers/cmake/vamos-buffers"
+vamos_buffers_BINARY_DIR  = "/home/ouldouz/ISTA/TASK;0/vamos/vamos-buffers"
+vamos_buffers_SOURCE_DIR  = "/home/ouldouz/ISTA/TASK;0/vamos/vamos-buffers"
+
+vamos_buffers_LIBRARIES_DIRS_core = "/home/ouldouz/ISTA/TASK 0/vamos/vamos-buffers/src/core"
+vamos_buffers_LIBRARIES_DIRS_shmbuf = "/home/ouldouz/ISTA/TASK 0/vamos/vamos-buffers/src/shmbuf"
+vamos_buffers_LIBRARIES_DIRS_streams = "/home/ouldouz/ISTA/TASK 0/vamos/vamos-buffers/src/streams"
+vamos_buffers_INCLUDE_DIR = "/home/ouldouz/ISTA/TASK 0/vamos/vamos-buffers/cmake/vamos-buffers/../../include"
+
+vamos_buffers_BUILD_TYPE               = "RelWithDebInfo"
+vamos_buffers_C_COMPILER               = "/usr/bin/cc"
+
+vamos_buffers_OPTION_ENABLE_IPO        = "ON".upper()
+
+vamos_hyper_DIR         = "/home/ouldouz/ISTA/TASK 0/vamos/vamos-hyper/cmake/vamos-hyper"
+vamos_hyper_BINARY_DIR  = ""
+vamos_hyper_SOURCE_DIR  = ""
+vamos_hyper_INCLUDE_DIR = "/home/ouldouz/ISTA/TASK 0/vamos/vamos-hyper/cmake/vamos-hyper/../../include"
+
+vamos_hyper_BUILD_TYPE               = ""
+vamos_hyper_C_COMPILER               = ""
+
+vamos_hyper_OPTION_ENABLE_IPO        = "".upper()
+
+vamos_sources_DIR         = "/home/ouldouz/ISTA/TASK 0/vamos/vamos-sources/cmake/vamos-sources"
+vamos_sources_BINARY_DIR  = "/home/ouldouz/ISTA/TASK;0/vamos/vamos-sources"
+vamos_sources_SOURCE_DIR  = "/home/ouldouz/ISTA/TASK;0/vamos/vamos-sources"
+vamos_sources_PYTHONPATH  = "/home/ouldouz/ISTA/TASK 0/vamos/vamos-sources/python"
+
+
+vamos_common_PYTHONPATH  = "/home/ouldouz/ISTA/TASK 0/vamos/vamos-common/python"
+
---
+++ config.sh
@@ -0,0 +1,5 @@
+vamos_buffers_LIBRARIES_DIRS_core="/home/ouldouz/ISTA/TASK 0/vamos/vamos-buffers/src/core"
+vamos_buffers_LIBRARIES_DIRS_shmbuf="/home/ouldouz/ISTA/TASK 0/vamos/vamos-buffers/src/shmbuf"
+vamos_buffers_LIBRARIES_DIRS_streams="/home/ouldouz/ISTA/TASK 0/vamos/vamos-buffers/src/streams"
+vamos_buffers_INCLUDE_DIR="/home/ouldouz/ISTA/TASK 0/vamos/vamos-buffers/cmake/vamos-buffers/../../include"
+vamos_buffers_BUILD_TYPE="RelWithDebInfo"
