1edba0e6c279527d0cfab83140eb028b478a443f
Ouldouz-Neysari <125090068+Ouldouz-Neysari@users.noreply.github.com>
2023-08-23 13:09:07 +0330

    Add files via upload

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ouldouz/ISTA/TASK 0/vamos/vamos-mpt
+# Build directory: /home/ouldouz/ISTA/TASK 0/vamos/vamos-mpt
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
